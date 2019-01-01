dd937a5c827580912ec81d3e209ae96074623301
Gyorgy Kovacs <gyuriofkovacs@gmail.com>
2019-01-01 17:36:23 +0100

    py version added

--- utils/docs.py
+++ utils/docs.py
@@ -367,8 +367,8 @@ def create_readme_page():
 .. |ReadTheDocs| image:: https://readthedocs.org/projects/smote-variants/badge/?version=latest
 .. _ReadTheDocs: https://smote-variants.readthedocs.io/en/latest/?badge=latest
 
-.. |PythonVersion| image:: https://img.shields.io/badge/python-3.5%20%7C%203.6%20%7C%203.7-blue.svg
-.. _PythonVersion: https://img.shields.io/badge/python-3.5%20%7C%203.6%20%7C%203.7-blue.svg
+.. |PythonVersion| image:: https://img.shields.io/badge/python-3.5%20%7C%203.6%20%7C%203.7-green.svg
+.. _PythonVersion: https://img.shields.io/badge/python-3.5%20%7C%203.6%20%7C%203.7-green.svg
 
 .. |PyPi| image:: https://badge.fury.io/py/smote-variants.svg
 .. _PyPi: https://badge.fury.io/py/smote-variants
